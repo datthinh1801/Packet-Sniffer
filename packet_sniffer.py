4ee8ace2d7cedcee0e137011b889d925444bb1e0
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-05-01 04:47:42 -0400

    Add filter for HTTP and Raw layers

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,9 +1,16 @@
 #! /bin/python3
 import scapy.all as scapy
+from scapy.layers import http
 
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=lambda x: x.show())
+    scapy.sniff(iface=interface, store=False, prn=process_packet)
+
+
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
 
 
 sniff("eth0")
